465710e3846d6cb8df2f04be91022c1cbafe2772
GiomarMC <gmunozcu@unsa.edu.pe>
2023-04-09 11:26:52 -0500

    Se agrega inicio de proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/GiomarMC/MILLIONAIRE_PROJECT/main/Numeros_Ganadores.csv")
+df = pd.read_csv("./Numeros_Ganadores.csv")
 print(df)
