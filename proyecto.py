96f4771d8ddb8460f3c0556eae461d9a2fd43016
GiomarMC <gmunozcu@unsa.edu.pe>
2023-04-14 11:02:15 -0500

    Se Cambia codigo y se crea dos formatos de frecuencia

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv("https://raw.githubusercontent.com/GiomarMC/MILLIONAIRE_PROJECT/main/Numeros_Ganadores.csv?token=GHSAT0AAAAAACBFKO3SVVZYXE4RP2FBQVF2ZBS3W6A")
+df = pd.read_csv("https://raw.githubusercontent.com/GiomarMC/MILLIONAIRE_PROJECT/main/Numeros_Ganadores.csv")
+df = df[['NG 1','NG 2','NG 3','NG4 ','NG 5','NG 6']]
+df.columns = ['NG_1','NG_2','NG_3','NG_4','NG_5','NG_6']
 print(df)
+orden = pd.DataFrame()
+orden['Numeros'] = range(1,47)
+for i in df.columns:
+    lista = []
+    for j in orden.Numeros:
+        try:
+            lista.append(df[i].value_counts()[j])
+        except KeyError:
+            lista.append(0)
+    orden[i] = lista
+print(orden)
+orden.to_excel("Frecuencia_NumerosG.xlsx")
+orden.to_csv("Frecuencia_NumerosG.csv")
